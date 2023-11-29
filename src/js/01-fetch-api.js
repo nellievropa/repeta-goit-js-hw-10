e02715c5c3b785f8bcb7f4fc48808a2f5f1ab959
nellievropa <nellievropa23@gmail.com>
2023-11-29 14:12:33 +0200

    pockemon

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -1,13 +1,15 @@
 import '../css/common.css';
 
-// fetch()
+
+// const a = "0";
+// const b = a ?? "b";
+// const c = a || "c";
+
+
+// console.log('hello world')
+
+const r = fetch('https://pokeapi.co/api/v2/ability/5')
+console.log(r);
+
 
 // https://pokeapi.co/
-
-const a = "0";
-const b = a ?? "b";
-const c = a || "c";
-
-
-console.log('hello world')
-
