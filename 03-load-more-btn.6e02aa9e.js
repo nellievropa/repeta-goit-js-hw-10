03bec5382919976aa283650d252fc6a64f9e9282
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-12-07 12:32:20 +0000

    Deploying to gh-pages from @ nellievropa/repeta-goit-js-hw-10@88b4ae930703de9da6e148d623a1cbfc3b79f6d5 🚀

---
+++ 03-load-more-btn.6e02aa9e.js
@@ -0,0 +1,2 @@
+fetch("https://newsapi.org/v2/everything?q=cat&language=en&pageSize=8&page=1",{headers:{Authorization:"15038c1cf1024f0f88ddd0a11fc0f440"}}).then((e=>e.json)).then(console.log);
+//# sourceMappingURL=03-load-more-btn.6e02aa9e.js.map
---
+++ 03-load-more-btn.c98f37a1.js
@@ -0,0 +1,2 @@
+fetch("https://newsapi.org/v2/everything?q=cat&language=en&pageSize=8&page=1",{headers:{Authorization:"15038c1cf1024f0f88ddd0a11fc0f440"}}).then((function(e){return e.json})).then(console.log);
+//# sourceMappingURL=03-load-more-btn.c98f37a1.js.map
