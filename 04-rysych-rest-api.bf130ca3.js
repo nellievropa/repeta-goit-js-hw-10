0f51d5f71711f354b6560ee65f52b8d5dbf32a0e
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-12-12 15:07:00 +0000

    Deploying to gh-pages from @ nellievropa/repeta-goit-js-hw-10@425d2e218aa43722ea91c7dfc3992485f2eec31a 🚀

---
+++ 04-rysych-rest-api.bf130ca3.js
@@ -0,0 +1,2 @@
+fetch("https://swapi.dev/api/people").then((function(e){return e.json()})).then(console.log),fetch("http://api.weatherapi.com/v1").then((function(e){return e.json()})).then(console.log);
+//# sourceMappingURL=04-rysych-rest-api.bf130ca3.js.map
---
+++ 04-rysych-rest-api.f6bba91a.js
@@ -0,0 +1,2 @@
+fetch("https://swapi.dev/api/people").then((e=>e.json())).then(console.log),fetch("http://api.weatherapi.com/v1").then((e=>e.json())).then(console.log);
+//# sourceMappingURL=04-rysych-rest-api.f6bba91a.js.map
