4d7f5e72591e6adf8cdbff07d5a1a3b96290a862
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-12-19 15:01:25 +0000

    Deploying to gh-pages from @ nellievropa/repeta-goit-js-hw-10@820487add38adad4044289ccebdf5bab1c1ea7e6 🚀

--- 04-rysych-rest-api.e44a389f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-search").addEventListener("submit",(function(e){e.preventDefault();const{query:t,days:o}=e.currentTarget.elements;(function(e,t){return fetch(`http://api.weatherapi.com/v1/forecast.json?key=bc046e36b7d642a2a0294826231312&q=${e}&days=${t}`).then((e=>{if(!e.ok)throw new Error(e.statusText);return e.json()}))})(t.value,o.value).then((e=>console.log(e))).catch((e=>console.log(e))),console.log(t),console.log(o)}));
-//# sourceMappingURL=04-rysych-rest-api.e44a389f.js.map
