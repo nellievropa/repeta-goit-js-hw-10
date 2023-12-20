451b06bc4b601b7edb2b4a5fa65f9abce76f1ce6
nellievropa <nellievropa23@gmail.com>
2023-12-20 16:42:12 +0200

    forecast ready

--- src/js/04-rysych-rest-api.js
+++ src/js/04-rysych-rest-api.js
@@ -16,22 +16,22 @@ function onShowWeather(evt) {
     // робимо деструктуризацію по name
     const { query, days } = evt.currentTarget.elements;
     getWeather(query.value, days.value)
-    .then(data => console.log(data))
+    .then(data => (list.innerHTML = createMarkup(data.forecast.forecastday)))
     .catch(error => console.log(error));
     console.log(query);
     console.log(days);
 }
 
 
-function createMarkup(array) {
+// function createMarkup(array) {
   
-    // return array.map( => `<li>
-    // <img src="" alt="">
-    //         <p></p>
-    //     <h2></h2>
-    //     <h3></h3>
-    // </li>`).join('')
-}
+//     return array.map( => `<li>
+//     <img src="" alt="">
+//             <p></p>
+//         <h2></h2>
+//         <h3></h3>
+//     </li>`).join('')
+// }
 
 function getWeather(city, days) {
     // http://api.weatherapi.com/v1/forecast.json?key=bc046e36b7d642a2a0294826231312&q=Paris&days=7
@@ -49,11 +49,27 @@ function getWeather(city, days) {
     });
   
 }
-// викликаємо функцію і парсимо її далі!!! 
+// викликаємо функцію і парсимо її далі!!!
 // getWeather('Lviv', 7)
 //     .then(data => console.log(data))
 //     .catch(error => console.log(error));
 // у відповіді ми отримаємо Ок: зі статусом тру або фальс
 // і його треба зразу опрацювати, щоб знати той статус, і робиться це в першому then
 // якщо відповідь не Ок, робимо примусову помилку зі статусом із Responce
-    // if (!responce.ok) { throw new Error(responce.statusText)}
+// if (!responce.ok) { throw new Error(responce.statusText)}
+    
+// створюємо функцію, яка дасть нам розмітку нашої картки - в якій ми побачимо відповідь на наш запит
+
+function createMarkup(arr) {
+// приклад вкладеної деструктуризації
+return arr.map(({date, day:{avgtemp_c, condition:{text, icon} }}) => `<li>
+        <img src="${icon}" alt="${text}">
+        <p>${text}</p>
+        <h2>${date}</h2>
+        <h3>${avgtemp_c}</h3>
+    </li>`).join('')
+}
+
+// переклад з англійської на українську
+
+// mapping =[{input : 'київ', output : 'kiev' }]
