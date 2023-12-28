b2c64bc91efe91da218769c9dab08343d83fc46f
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-12-28 13:42:06 +0000

    Deploying to gh-pages from @ nellievropa/repeta-goit-js-hw-10@0d1a479c1e6a6862ebff5dbd0e2089106aed5e0e 🚀

--- 05-scroll.018b4610.js
+++
@@ -1,2 +0,0 @@
-var BASE_URL="https://the-one-api.dev/v2",END_POINT="character",KEY="fP9PYRN - X34XoDXpvVl1";function getCharacter(){var t=new URLSearchParams({limit:20,page:1}),a={method:"GET",headers:{Authorization:"Bearer".concat(KEY)}};fetch("".concat(BASE_URL).concat(END_POINT,"?").concat(t),a).then((function(t){return console.log(t)}))}getCharacter();
-//# sourceMappingURL=05-scroll.018b4610.js.map
